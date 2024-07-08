11bb81701f873ad32cbcbfa03ca8b968fef877b7
Rahul Sureshrao Chavan <rahul.chavan@capgemini.com>
2024-07-08 11:28:40 +0530

    Update Home.js
    Changed title from home componnet

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h3>SCB Digital School - React (Week 2)</h3>
+      <h3>SCB Digital School - React (Week 2).</h3>
       <hr />
     </div>
   )

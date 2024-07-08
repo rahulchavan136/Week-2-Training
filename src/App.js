b722cc7c95b78096a52214fd2b59e48a6ee2edb0
Your Name <your.email@example.com>
2024-07-08 11:12:46 +0530

    week-2 training started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import  Home  from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Udemy Course : Advanced React And Redux</h1>
-      <hr />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h3>SCB Digital School - React (Week 2)</h3>
+      <hr />
+    </div>
+  )
+}
+export default Home;
